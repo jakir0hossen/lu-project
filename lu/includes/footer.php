99dad7206f8644d56fff38da6afa446d445001a7
jakir0hossen <jhbjakir@gmail.com>
2025-01-25 17:30:00 +0600

    Admin

---
+++ lu/includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5 bg-success">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Lu Magazine <?php echo date('Y');?></p>
+      </div>
+      <!-- /.container -->
+    </footer>
